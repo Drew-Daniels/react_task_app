e33e374b5d2cb36db8766b7b1996a3f705ea606f
Drew Daniels <drew@thedaniels.com>
2022-02-11 11:50:42 -0600

    Finished first version of react task app.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Overview from './components/Overview';
+import uniqid from 'uniqid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// TODO:
+// 1.) Manage the amount of tasks in state and using that to display the task number next to each task - without using ordered lists
+// 2.) Implement a delete button for each task
+// 3.) Implement an edit button for each task - when the edit button is clicked, the specific task should become changeable and the edit button should change to a resubmit button
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      task: {
+        text: '',
+        id: uniqid(),
+      },
+      tasks: [],
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      task: {
+        text: e.target.value,
+        id: this.state.task.id,
+      },
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: {
+        text: '',
+        id: uniqid()
+      },
+    });
+  };
+
+  render() {
+    const {task, tasks} = this.state;
+
+    return (
+    <div>
+      <form onSubmit={this.onSubmitTask}>
+        <label htmlFor="taskInput">Enter task</label>
+        <input 
+          onChange = {this.handleChange}
+          value = {task.text}
+          type="text" 
+          id="taskInput"
+          />
+        <button type="submit">
+          Add Task
+        </button>
+      </form>
+      <Overview tasks={tasks} />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
